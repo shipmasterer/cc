d255cf3e971e92b290cc3fd1db0686f79b2deb44
(0^0) <2296754769@qq.com>
2025-04-17 23:06:05 +0800

    for

--- code/2.c++
+++ code/2.c++
@@ -2,6 +2,6 @@
 using namespace std;
 int main()
 {
-    cout << "ÎÒaewdaef" << endl;
+    cout << "ÎÞaewdaef" << endl;
     return 0;
 }
---
+++ code/for.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i;
+    for (i = 0; i <= 5; i++)
+    {
+        /* code */
+        cout << i << "\n"
+             << endl;
+    }
+
+    cout << "ÎÞaewdaef" << endl;
+    return 0;
+}
