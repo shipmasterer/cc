2ee717fc6a833206f4d12d206900a7d3d665aef1
(0^0) <2296754769@qq.com>
2025-04-17 21:52:33 +0800

    code

---
+++ code/cin&&cout.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int a;
+     cin >> a;
+     cout << a << "\n"
+          << endl;
+     cout << "aaewdaef" << "\n"
+          << endl;
+     return 0;
+}
