be3873425bfa2b1011fc98275d65a8f56be4d542
(0^0) <2296754769@qq.com>
2025-04-20 20:00:36 +0800

    枚举

---
+++ code/枚举.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    enum weekdays
+    {
+        Monday,
+        Tuesday,
+        Wednesday
+    };
+    weekdays today;
+
+    today = Monday;
+    cout << today << "\n";
+
+    today = Tuesday;
+    cout << today << "\n";
+}
